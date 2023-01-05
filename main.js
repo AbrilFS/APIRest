bb9de123a707e394503ad2ccfd6ec57e8aca235b
Abril Flores Silva <abril.qfs@gmail.comgit remote add origin git@github.com:AbrilFS/preworkMAC.gitgithhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhellohow areyoudoingtoday!>
2023-01-05 17:50:09 -0600

    métodos GET y POST

--- main.js
+++ main.js
@@ -1,19 +1,43 @@
 
 //1. creamos una variable const donde gaurdaremos la URL que queremos jalar.
+const API_URL_RANDOM = " https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_9apQSZpNSuP47RFiTEXCT9mqGiHTa71dHtpG6LDSh8i43mRfQqieBcvDAkuU6yR9";
 
-const URL = " https://api.thecatapi.com/v1/images/search";
+const API_URL_FAV = " https://api.thecatapi.com/v1/images/favourites?limit=2&api_key=live_9apQSZpNSuP47RFiTEXCT9mqGiHTa71dHtpG6LDSh8i43mRfQqieBcvDAkuU6yR9";
 
-//2. Ponemos la URL como argumento de la función fetch. Esta llamada nos devuelve una promesa.
-fetch (URL)
- //3. El método then() de esa promesa nos entrega un objeto response.
- //A partir de este objeto llamamos al método json() para obtener el cuerpo de la respuesta. Este método json() nos devuelve otra promesa que se resolverá cuando se haya obtenido el contenido.
-    .then (res => res.json())
- //4. El método then() de esta segunda promesa recibe el cuerpo devuelto por la promesa anterior 
-    .then(data => {
-    const img = document.querySelector('img'); //tomamos el nodo img del HTML mediante el DOM
+const spanError= document.getElementById("error")
 
-    img.src = data[0].url; //le decimos que el src de la img, sea la url del objeto json.
+ async function loadRandomMichis(){
+    const res = await fetch (API_URL_RANDOM); //llamamos a la API
+    //convertimos en sintaxis que JS pueda entender con .json
+    const data= await res.json();
     
- });
+        console.log("Random");
+        console.log(data)
 
- 
+        if(res.status !== 200){ //condicional para mostrar errores de HTTP
+            spanError.innerHTML="Hubo un error" + res.status;
+        } else {
+            const img1 = document.getElementById('img1'); //tomamos el nodo img del HTML mediante el DOM para renderizar.
+            const img2 = document.getElementById('img2');
+        
+            img1.src = data[0].url; //le decimos que el src de la img, sea la url del objeto json.
+            img2.src = data[1].url;
+        }     
+    }
+
+ async function loadFavMichis(){
+    const res = await fetch (API_URL_FAV); 
+    const data= await res.json();
+            console.log("Favoritos")
+            console.log(data)
+    
+    
+    if (res.status !== 200) {
+        spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+        }
+    
+    }
+
+ loadRandomMichis(); //llamamos a la función para que la img no nos salga vacía al entrar a la página.
+
+ loadFavMichis();
